52b95185f83f7b3901e59dfff231f467e25a281a
Steve Reynolds <steve.reynolds@blackpear.com>
2015-09-10 16:10:45 +0100

    update

--- standardInstall.sh
+++ standardInstall.sh
@@ -2,6 +2,9 @@
 username = $1
 emailaddress = $2
 
+#change to home folder
+cd $HOME
+
 #install node
 sudo apt-get update
 curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
@@ -19,7 +22,7 @@ sudo apt-get install --yes build-essential libssl-dev libcurl4-gnutls-dev libexp
 wget https://github.com/git/git/archive/v2.5.0.zip
 unzip v2.5.0.zip
 cd git-*
-#make prefix=/usr/local all
+make prefix=/usr/local all
 #sudo make prefix=/usr/local install
 #git config --global user.name $username
 #git config --global user.email $emailaddress
