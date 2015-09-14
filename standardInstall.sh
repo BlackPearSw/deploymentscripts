5eb4d9b81738b970b7c4316da3bb84427e33538b
Steve Reynolds <steve.reynolds@blackpear.com>
2015-09-14 14:25:03 +0100

    update

--- standardInstall.sh
+++ standardInstall.sh
@@ -20,10 +20,10 @@ su - $uname -c "npm set $npma"
 su - $uname -c "npm set always-auth true"
 
 #install nginx
-#sudo nginx=stable
-#sudo add-apt-repository ppa:nginx/$nginx
-#sudo apt-get update
-#sudo apt-get install nginx
+sudo nginx=stable
+sudo add-apt-repository ppa:nginx/$nginx
+sudo apt-get update
+sudo apt-get install nginx
 
 #install pm2
 sudo npm install pm2 -g
