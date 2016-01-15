fc1d30f0a00a90ba2c0d3485ce8c5808a419bb71
Steve Reynolds <steve.reynolds@blackpear.com>
2016-01-15 16:28:59 +0000

    update

--- mongoInstall.sh
+++ mongoInstall.sh
@@ -54,11 +54,12 @@ then
 	#Install pm2 server monitor
 	su - $uname -c "pm2 install pm2-server-monit"
 
-	#install pm2 mongodb module
-	su - $uname -c "pm2 install pm2-mongodb"
-
 	#update pm2 mongodb ip
 	su - $uname -c "pm2 set pm2-mongodb:ip $privip"
+
+	#install pm2 mongodb module
+	su - $uname -c "pm2 install pm2-mongodb"
+	su - $uname -c "pm2 restart pm2-mongodb"
 
 	#link pm2 to key metrics
 	su - $uname -c "pm2 link $pm2pr $pm2pu $host"
