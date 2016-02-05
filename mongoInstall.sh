154f468641e489884897e1663ce69fca239e20e0
Steve Reynolds <steve.reynolds@blackpear.com>
2016-02-05 14:46:51 +0000

    Updated, see details.
    1) Updated MongoDB version
    2) Updated THP script to use official guidance
    3) Removed binding, so now defaults to 127.0.0.1
    4) Install pm2-mongodb module
    5) Create autossh user for ssh tunnel connections

--- mongoInstall.sh
+++ mongoInstall.sh
@@ -25,13 +25,8 @@ sudo apt-get install -y nodejs
 sudo npm install npm -g
 
 # Configure mongodb.list file with the correct location
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
-
-# Disable THP
-sudo echo never > /sys/kernel/mm/transparent_hugepage/enabled
-sudo echo never > /sys/kernel/mm/transparent_hugepage/defrag
-sudo grep -q -F 'transparent_hugepage=never' /etc/default/grub || echo 'transparent_hugepage=never' >> /etc/default/grub
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 
 # Install updates
 sudo apt-get -y update
@@ -39,14 +34,47 @@ sudo apt-get -y update
 #Install Mongo DB
 sudo apt-get install -y mongodb-org
 
+#Disable THP for Mongo DB
+sudo su -c "cat << EOF > /etc/init.d/disable-transparent-hugepages
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          disable-transparent-hugepages
+# Required-Start:    $local_fs
+# Required-Stop:
+# X-Start-Before:    mongod mongodb-mms-automation-agent
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Disable Linux transparent huge pages
+# Description:       Disable Linux transparent huge pages, to improve
+#                    database performance.
+### END INIT INFO
+
+case $1 in
+  start)
+    if [ -d /sys/kernel/mm/transparent_hugepage ]; then
+      thp_path=/sys/kernel/mm/transparent_hugepage
+    elif [ -d /sys/kernel/mm/redhat_transparent_hugepage ]; then
+      thp_path=/sys/kernel/mm/redhat_transparent_hugepage
+    else
+      return 0
+    fi
+
+    echo 'never' > ${thp_path}/enabled
+    echo 'never' > ${thp_path}/defrag
+
+    unset thp_path
+    ;;
+esac
+EOF"
+
+sudo chmod 755 /etc/init.d/disable-transparent-hugepages
+sudo update-rc.d disable-transparent-hugepages defaults
+
 #install pm2
 sudo npm install pm2 -g
 
 #Install pm2 server monitor
 su - $uname -c "pm2 install pm2-server-monit"
-
-#update pm2 mongodb ip
-su - $uname -c "pm2 set pm2-mongodb:ip $privip"
 
 #mount data disk
 hdd="/dev/sdc"
@@ -64,11 +92,19 @@ sudo mkdir /datadrive/mongodb
 sudo chown mongodb:mongodb /datadrive/mongodb
 
 #update mongo config
-sudo sed -i "/bindIp/s/127.0.0.1/$privip/" /etc/mongod.conf
 sudo sed -i "/dbPath/s/var\/lib/datadrive/" /etc/mongod.conf
 
 #restart mongodb
 sudo service mongod restart
+
+#Install pm2 mongodb module
+su - $uname -c "pm2 install pm2-mongodb"
+
+#create autossh user and create empty authorised_keys file
+sudo adduser --system --group --shell /bin/false --disabled-password autossh
+sudo mkdir -p /home/autossh/.ssh
+sudo touch /home/autossh/.ssh/authorized_keys
+sudo chown -R autossh:autossh /home/autossh/.ssh
 
 #link pm2 to keymetrics
 if [ "$keymet" = "y" ]
