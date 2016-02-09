61d35a688d7648c6ce9a15a5ccdfc9b3dd52b357
Steve Reynolds <steve.reynolds@blackpear.com>
2016-02-09 16:48:51 +0000

    update

--- standardInstall.sh
+++ standardInstall.sh
@@ -36,14 +36,14 @@ then
 	sudo apt-get update
 	sudo apt-get install --yes autossh
 	sudo su -c "cat << EOF > /etc/init/autossh.sh
-	description "Start autossh to control ssh tunnel"
-	author "Steve Reynolds"
+	description \"Start autossh to control ssh tunnel\"
+	author \"Steve Reynolds\"
 	start on (local-filesystems and net-device-up IFACE=eth0)
 	stop on runlevel [016]
 	setuid autossh
 	respawn
 	respawn limit 5 60
-	exec autossh -M 0 -N -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -L $sshport:localhost:27017 -i /home/autossh/.ssh/id_rsa autossh@$dbhost
+	exec autossh -M 0 -N -o \"ServerAliveInterval 60\" -o \"ServerAliveCountMax 3\" -L $sshport:localhost:27017 -i /home/autossh/.ssh/id_rsa autossh@$dbhost
 	EOF"
 	sudo service autossh start
 fi
